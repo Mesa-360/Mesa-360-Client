7cf656e8b6dad2a26f67a424e22c276e046d13ff
Tomas Padilla <padillatomasagustin@gmail.com>
2025-03-19 08:01:51 +0100

    develop - Project clone

---
+++ app/(protected)/mesas/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { SalonesTabs } from "./_components";
+
+const MesasPage = () => {
+  return (
+    <section className="lg:min-w-[900px] max-w-[1300px] flex flex-col items-center justify-center m-auto gap-6">
+      <article className="flex flex-col gap-2 items-center text-center">
+        <h1 className="text-3xl font-bold mb-2">📍 Mapa de Mesas</h1>
+        <p className="text-gray-600 max-w-md">
+          Organiza y ajusta la distribución de las mesas de tu restaurante de
+          forma fácil e intuitiva.
+        </p>
+      </article>
+      <SalonesTabs></SalonesTabs>
+    </section>
+  );
+};
+
+export default MesasPage;
---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  images: {
+    unoptimized: true,
+    domains: ["upload.wikimedia.org", "pexels.com", "unsplash.com"], // Agrega los dominios usados en tus imágenes
+  },
+  reactStrictMode: false,
+};
+
+export default nextConfig;
